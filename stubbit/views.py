b35adcb8bd3dfb264cddc14bd3381cf70b32220a
Tuco Regel <thomas@thewilberdinggroup.com>
2022-02-23 09:12:35 -0500

    Revert "Merge branch 'main' of https://github.com/Zackmoody/CSI4999"
    This reverts commit da96d6ec4d1699e5516f45b4140bc7f4daf5140f, reversing
    changes made to 4dfedbafb1ecd1157a935e370e59f9d568e3e8a8.

--- stubbit/views.py
+++ stubbit/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import *
 # Create your views here.
 
 def home(request):
-
-    all_stubs = Stub.objects.all()
-    user = Account.objects.get(pk=1)
-    user_stubs = user.stub_set.all()
-    dict = {'all_stubs':all_stubs, 'user_stubs':user_stubs}
-    return render(request, 'home.html', dict)
+    return render(request, 'home.html')
